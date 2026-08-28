04c6c3d235eaae9508db3f657300a5bf1889b954
Chawlucha-hub <liam.ahlberg2008@gmail.com>
2026-08-28 11:03:59 +0200

    Update WorldPhysics.cs

--- 2d fysik motor/WorldPhysics.cs
+++ 2d fysik motor/WorldPhysics.cs
@@ -8,5 +8,53 @@ namespace _2d_fysik_motor
 {
     internal class WorldPhysics
     {
+        public List<PhysicsObject> Bodies { get; } = new List<PhysicsObject>();
+        public Vector2D Gravity { get; set; } = new Vector2D(0, 980f); // Standard-gravitation
+
+        // Lägg till ett nytt objekt i motorn
+        public PhysicsObject AddBody(Vector2D position, float mass)
+        {
+            PhysicsObject body = new PhysicsObject(position, mass);
+            Bodies.Add(body);
+            return body;
+        }
+
+        // Uppdatera alla objekt i världen
+        public void Step(float deltaTime, float screenWidth, float screenHeight, float radius)
+        {
+            foreach (var body in Bodies)
+            {
+                // 1. Lägg på gravitation
+                body.AddForce(Gravity * body.Mass);
+
+                // 2. Uppdatera position och hastighet
+                body.Update(deltaTime);
+
+                // 3. Enkel vägg- och golvkollision för skärmkanterna
+                HandleScreenBoundaries(body, screenWidth, screenHeight, radius);
+            }
+        }
+
+        private void HandleScreenBoundaries(PhysicsObject body, float width, float height, float radius)
+        {
+            // Golv
+            if (body.Position.Y >= height - radius)
+            {
+                body.Position.Y = height - radius;
+                body.Velocity.Y *= -body.Restitution;
+            }
+            // Vänster vägg
+            if (body.Position.X <= radius)
+            {
+                body.Position.X = radius;
+                body.Velocity.X *= -body.Restitution;
+            }
+            // Höger vägg
+            if (body.Position.X >= width - radius)
+            {
+                body.Position.X = width - radius;
+                body.Velocity.X *= -body.Restitution;
+            }
+        }
     }
 }
